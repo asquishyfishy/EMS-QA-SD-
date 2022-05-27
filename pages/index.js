1a7d611243ada7a890e116daaeb92530cad072dc
asquishyfishy <timothy.sereda@trojans.dsu.edu>
2022-05-26 20:55:51 -0500

    WSL

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Gotted started by editing <code>pages/index.js</code>
+          Gotted (now in WSL) started by editing <code>pages/index.js</code>
         </p>
       </main>
 
